724ffbf15b38abc0912f3b672b316cb4c45e667b
aahcbird <eniaslovy2@naver.com>
2019-05-03 20:26:44 +0900

    driver initializer with exception + dynamic allocation

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 #include "connect_four.h"
 
-ConnectFour InitializeConnectFour() {
-	ConnectFour cf;
+ConnectFour* InitializeConnectFour() {
+	ConnectFour *cf = new ConnectFour();
 
 	while (true) {
 		try {
-			if (cf.Initialize()) return cf;
+			if (cf->Initialize()) return cf;
 		}
 		catch (NumberFormatException e) {
-			cf.set_error_message("Error : Number value required!\n");
+			cf->set_error_message("Error : Number value required!\n");
 		}
 		catch (SelectOutOfRangeException e) {
-			cf.set_error_message("Error : Input out of range! (1-2)\n");
+			cf->set_error_message("Error : Input out of range! (1-2)\n");
 		}
 		catch (IllegalInputException e) {
-			cf.set_error_message("Error : Unknown input error!\n");
+			cf->set_error_message("Error : Unknown input error!\n");
 		}
 	}
 }
 
 void StartConnectFour() {
-	ConnectFour cf = InitializeConnectFour();
+	ConnectFour *cf = InitializeConnectFour();
+	printf("Game Over\n");
 }
 
 int main() {
